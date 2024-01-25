2d3d14c6fcc4e73be1806ab37d8f1addad78afb2
diecodev <diegodiaz05@gmail.com>
2024-01-25 16:06:41 -0500

    refactor: ⚡️ improve highlights cards and background overflow

--- app/components/root/bg.tsx
+++ app/components/root/bg.tsx
@@ -9,7 +9,7 @@ export const Background = () => {
   return (
     <div
       className={cn(
-        "absolute top-0 z-[-2] h-screen w-screen dark:bg-neutral-950 dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"
+        "absolute top-0 z-[-2] inset-0 dark:bg-neutral-950 dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"
       )}
     ></div>
   );
--- app/components/search/highlight-card.tsx
+++ app/components/search/highlight-card.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 export const HighlightCard = ({
   title,
   children,
-  footer,
+  footer = "",
 }: {
   title: string;
   children: ReactNode;
@@ -13,6 +13,6 @@ export const HighlightCard = ({
   <Card className="flex flex-col justify-between items-center gap-y-4">
     <Metric className="!text-lg text-center font-medium">{title}</Metric>
     {children}
-    {footer && <Text className="text-center">{footer}</Text>}
+    <Text className="text-center">{footer}</Text>
   </Card>
 );
