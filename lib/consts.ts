438ec2e8efa82b43a6dcaf32638877c74dbfa1d7
diecodev <diegodiaz05@gmail.com>
2024-01-25 15:09:03 -0500

    chore: ⚡️ create date formatter lib

--- lib/consts.ts
+++ lib/consts.ts
@@ -1,3 +1,4 @@
 // open weather api
-export const OPEN_WEATHER_URL = "https://api.openweathermap.org/geo/1.0/";
+export const OPEN_WEATHER_GEO_URL = "https://api.openweathermap.org/geo/1.0";
+export const OPEN_WEATHER_DATA_URL = "https://api.openweathermap.org/data/3.0";
 export const OPEN_WEATHER_KEY = process.env.OPEN_WEATHER_KEY ?? "";
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,25 @@ import { twMerge } from "tw-merge";
 export function cn(...input: ClassValue[]) {
   return twMerge(clsx(...input));
 }
+
+export function weekDays({
+  index,
+  short = false,
+}: {
+  index: number;
+  short?: boolean;
+}) {
+  if (!short) {
+    return [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ][index];
+  }
+
+  return ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][index];
+}
