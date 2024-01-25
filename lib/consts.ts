02947ff5d227b709af84676b5f0331e4f992e855
diecodev <diegodiaz05@gmail.com>
2024-01-25 03:26:13 -0500

    chore: 🧑‍💻 add intial utilities

---
+++ lib/consts.ts
@@ -0,0 +1,3 @@
+// open weather api
+export const OPEN_WEATHER_URL = "https://api.openweathermap.org/geo/1.0/";
+export const OPEN_WEATHER_KEY = process.env.OPEN_WEATHER_KEY ?? "";
---
+++ lib/utils.ts
@@ -0,0 +1,6 @@
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tw-merge";
+
+export function cn(...input: ClassValue[]) {
+  return twMerge(clsx(...input));
+}
